c279923dd79178d2b71bdb5aa117ca236ba457e1
damonzhang <damonzhang92@foxmail.com>
2018-08-09 11:30:05 +0800

    Update xmodem1k.h
    增加文件头注释

--- src/xmodem1k.h
+++ src/xmodem1k.h
@@ -1,34 +1,59 @@
-/****************************************Copyright (c)****************************************************
-**                            Guangzhou ZLGMCU Development Co., LTD
-**
-**                                 http://www.zlgmcu.com
-**
-**--------------File Info---------------------------------------------------------------------------------
-** File name:           XMODEM1K.h
-** Last modified Date:  2012-9-10
-** Last Version:        V1.0
-** Descriptions:        The XMODEM1K Communication Protocol
-**
-**--------------------------------------------------------------------------------------------------------
-** Created by:          CaoHua
-** Created date:        2012-9-10
-** Version:             V1.00
-** Descriptions:        整理应用程序
-**
-**--------------------------------------------------------------------------------------------------------
-** Modified by:         
-** Modified date:       
-** Version:             
-** Descriptions:        
-**
-**--------------------------------------------------------------------------------------------------------
-** Modified by:        
-** Modified date:      
-** Version:            
-** Descriptions:       
-**
-** Rechecked by:
-*********************************************************************************************************/
+/*******************************************************************************
+* -----------------------------------------------------------------------------
+*									     									 
+* xmodem1k.h - a simple xmodem 1k machine which support xmodem & xmodem 1K		     			 
+*									     
+* -----------------------------------------------------------------------------
+* Copyright (C) Damon Zhang
+* All rights reserved.
+*
+* Author : Damon Zhang
+* Website: https://damon-yun.github.io/blog.github.io/
+* E-mail : damoncheung@foxmail.com
+* -----------------------------------------------------------------------------
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* Code is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+* or GNU Library General Public License, as applicable, for more details.
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*
+* o Redistributions of source code must retain the above copyright notice, this list
+*   of conditions and the following disclaimer.
+*
+* o Redistributions in binary form must reproduce the above copyright notice, this
+*   list of conditions and the following disclaimer in the documentation and/or
+*   other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*******************************************************************************/
+
+/**
+ * \file
+ * \brief xmodem1K
+ * 
+ * \internal
+ * \par Modification History
+ * - 1.00 18-08-05  damon.zhang, first implementation.
+ * \endinternal
+ */
 #ifndef __XMODEM1K_H
 #define __XMODEM1K_H
 
